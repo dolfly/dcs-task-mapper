f460840b38a662b3913762a5e2d85f9b812a69c0
Heikki Orsila <heikki.orsila@iki.fi>
2010-12-20 15:28:55 +0200

    Cleanup coding style [CLEANUP]

--- arch.c
+++ arch.c
@@ -32,50 +32,49 @@
 
 struct ae_arch *ae_create_arch(void)
 {
-  struct ae_arch *arch;
-  if ((arch = calloc(1, sizeof(*arch))) == NULL)
-    ae_err("no memory for new arch\n");
-  return arch;
+	struct ae_arch *arch;
+	CALLOC_ARRAY(arch, 1);
+	return arch;
 }
 
 void ae_energy(double *A, double *statE, double *dynE,
 	       struct ae_mapping *map)
 {
-  int i;
-  double Atot = 0.0;
-  double fmax = 0;
-  struct ae_arch *arch = map->arch;
-  double dynP = 0.0;
-  double T = map->schedule->schedule_length;
-  double k = map->optimization->power_k;
+	int i;
+	double Atot = 0.0;
+	double fmax = 0;
+	struct ae_arch *arch = map->arch;
+	double dynP = 0.0;
+	double T = map->schedule->schedule_length;
+	double k = map->optimization->power_k;
 
-  assert(T > 0.0);
-  assert(k >= 0.0);
+	assert(T > 0.0);
+	assert(k >= 0.0);
 
-  for (i = 0; i < arch->npes; i++) {
-    struct ae_pe *pe = arch->pes[i];
+	for (i = 0; i < arch->npes; i++) {
+		struct ae_pe *pe = arch->pes[i];
 
-    Atot += pe->area;
-    fmax = MAX(fmax, pe->freq);
+		Atot += pe->area;
+		fmax = MAX(fmax, pe->freq);
 
-    dynP += pe->area * pe->freq * map->schedule->pe_utilisations[i];
-  }
+		dynP += pe->area * pe->freq * map->schedule->pe_utilisations[i];
+	}
 
-  for (i = 0; i < arch->nics; i++) {
-    struct ae_interconnect *ic = arch->ics[i];
+	for (i = 0; i < arch->nics; i++) {
+		struct ae_interconnect *ic = arch->ics[i];
 
-    Atot += ic->area;
-    fmax = MAX(fmax, ic->freq);
+		Atot += ic->area;
+		fmax = MAX(fmax, ic->freq);
 
-    dynP += ic->area * ic->freq * map->schedule->ic_utilisations[i];
-  }
+		dynP += ic->area * ic->freq * map->schedule->ic_utilisations[i];
+	}
 
-  if (A != NULL)
-    *A = Atot;
+	if (A != NULL)
+		*A = Atot;
 
-  if (statE != NULL)
-    *statE = T * Atot * fmax;
+	if (statE != NULL)
+		*statE = T * Atot * fmax;
 
-  if (dynE != NULL)
-    *dynE =  T * k * dynP;
+	if (dynE != NULL)
+		*dynE =  T * k * dynP;
 }
